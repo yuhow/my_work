a915a91172f9d541ad4fc9c27fd8ac55ebbd8021
You-Hao Chang <question0802@gmail.com>
2017-10-31 07:56:23 +0800

    Update my_NN_fitter.py

--- neural_network/my_NN_fitter.py
+++ neural_network/my_NN_fitter.py
@@ -35,7 +35,7 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     return outputs
 
 # global setting
-L2_NEURON = 5
+H1_NEURON = 5
 
 # Make up some real data and input fitting template(s)
 x_data = np.linspace(-1, 1, 50)[:, np.newaxis]  # np.newaxis adds a new dimension to x_data
@@ -57,18 +57,14 @@ y_data = np.square(x_data) + 0.5 + noise  # y = x*x + 0.5 + noise
 xs = tf.placeholder(tf.float32, [None, 3])
 ys = tf.placeholder(tf.float32, [None, 1])
 
-# Input -> hidden1 -> hidden2 -> output
-#   3   ->   10    ->   5     ->   1
-# add hidden layer (1 in and 10 out)
-l1 = add_layer(xs, 3, 5, activation_function=tf.nn.softplus)
-#l1 = add_layer(xs, 3, 10, activation_function=tf.nn.relu)
-
-# add hidden layer (10 in and 10 out)
-l2 = add_layer(l1, 5, L2_NEURON, activation_function=tf.nn.softplus)
-#l2 = add_layer(l1, 10, 5, activation_function=tf.nn.relu)
+# Input -> hidden1 -> output
+#   3   ->   5     ->   1
+# add hidden layer (3 in and 5 out)
+l1 = add_layer(xs, 3, H1_NEURON, activation_function=tf.nn.softplus)
+#l1 = add_layer(xs, 3, H1_NEURON,, activation_function=tf.nn.relu)
 
 # add output layer (10 in and 1 out)
-prediction = add_layer(l2, L2_NEURON, 1, activation_function=None)
+prediction = add_layer(l1, H1_NEURON, 1, activation_function=None)
 
 # the error between prediciton and real data
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
@@ -87,9 +83,9 @@ fig.patch.set_facecolor('bisque')
 ax_in = []
 for k_idx in range(3):
     ax_in.append(fig.add_subplot(5, 3, k_idx*3+4))
-ax_l2 = []
-for k_idx in range(L2_NEURON):
-    ax_l2.append(fig.add_subplot(5, 3, k_idx*3+2))
+ax_h1 = []
+for k_idx in range(H1_NEURON):
+    ax_h1.append(fig.add_subplot(5, 3, k_idx*3+2))
 ax = fig.add_subplot(1, 3, 3)
 frame_seq = []
 
@@ -112,27 +108,27 @@ for i in range(10000):
         except Exception:
             pass
 
-        l2_kernels = sess.run(l2, feed_dict={xs: input_template})
+        h1_kernels = sess.run(l1, feed_dict={xs: input_template})
         prediction_value = sess.run(prediction, feed_dict={xs: input_template})
 
-        # l2 output
-        l2_output = []
-        for k_idx in range(l2_kernels.shape[1]):
+        # h1 output
+        all_output = []
+        for k_idx in range(h1_kernels.shape[1]):
             if i == 0:
-                plot_tmp, = ax_l2[k_idx].plot(x_data, l2_kernels[:, k_idx], c='blue', label='kernel_2-'+str(k_idx+1))
+                plot_tmp, = ax_h1[k_idx].plot(x_data, h1_kernels[:, k_idx], c='blue', label='kernel_h1-'+str(k_idx+1))
             else:
-                plot_tmp, = ax_l2[k_idx].plot(x_data, l2_kernels[:, k_idx], c='blue')
-            ax_l2[k_idx].legend(loc="upper right", prop={'size': 10})
-            l2_output.append(plot_tmp)
+                plot_tmp, = ax_h1[k_idx].plot(x_data, h1_kernels[:, k_idx], c='blue')
+            ax_h1[k_idx].legend(loc="upper right", prop={'size': 10})
+            all_output.append(plot_tmp)
         
         # plot the prediction
         #plt.pause(0.1)
         #frame_seq.append(plt.plot(x_data, prediction_value, 'r-', lw=5))
         ax.set_xlim((np.amin(x_data)-abs(np.amin(x_data)*0.1), np.amax(x_data)+abs(np.amax(x_data)*0.1)))
-        ax.set_ylim((np.amin(y_data[0])-1, np.amax(y_data[0])+1))
+        ax.set_ylim((np.amin(y_data)-abs(np.amin(y_data)*0.1), np.amax(y_data)+abs(np.amax(y_data)*0.1)))
         plot_output, = ax.plot(x_data, prediction_value, 'r.')
-        l2_output.append(plot_output)
-        frame_seq.append(tuple(l2_output))
+        all_output.append(plot_output)
+        frame_seq.append(tuple(all_output))
 #        frame_seq.append(ax.plot(x_data, prediction_value, 'r.'))
 
 ani = animation.ArtistAnimation(fig, frame_seq, interval=250, blit=True, repeat=False)
@@ -144,4 +140,3 @@ plt.tight_layout()
 plt.subplots_adjust(wspace = 1)
 
 plt.show()
-
